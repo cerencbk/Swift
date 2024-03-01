7488fc3de1f249b799281591dea991ab31577601
Ceren Çabık <ceren.cbk@hotmail.com>
2024-03-01 14:53:18 +0300

    update

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var myNumber = 1
+myNumber+=1
+
+
+var number = 0
+
+//While loop
+
+while number <= 10 {
+
+    number += 1
+}
+
+var caracterAlive = true
+
+while caracterAlive == true{
+    caracterAlive = false
+    
+}
+
+//For loop
+
+var myFruit = ["Banana", "Apple", "Orange"]
+for fruit in myFruit{
+    print(fruit)
+}
