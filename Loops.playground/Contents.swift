c45fa4f1b8eec8ec69043e14a5bc09c28075b8dd
Ceren Çabık <ceren.cbk@hotmail.com>
2024-03-01 16:13:44 +0300

    update

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -1,28 +1,47 @@
 import UIKit
+//
+//var myNumber = 1
+//myNumber+=1
+//
+//
+//var number = 0
+//
+////While loop
+//
+//while number <= 10 {
+//
+//    number += 1
+//}
+//
+//var caracterAlive = true
+//
+//while caracterAlive == true{
+//    caracterAlive = false
+//    
+//}
+//
+////For loop
+//
+//var myFruit = ["Banana", "Apple", "Orange"]
+//for fruit in myFruit{
+//    print(fruit)
+//}
 
-var myNumber = 1
-myNumber+=1
+/// If kontrolleri
+
+//var myAge = 24
+//
+//if myAge < 30 {
+//    print("30-")
+//}
+//else if myAge > 30 && myAge < 40{
+//    print("30s")
+//}
+//else{
+//    print("40s")
+//}
 
 
-var number = 0
+//Function
 
-//While loop
 
-while number <= 10 {
-
-    number += 1
-}
-
-var caracterAlive = true
-
-while caracterAlive == true{
-    caracterAlive = false
-    
-}
-
-//For loop
-
-var myFruit = ["Banana", "Apple", "Orange"]
-for fruit in myFruit{
-    print(fruit)
-}
