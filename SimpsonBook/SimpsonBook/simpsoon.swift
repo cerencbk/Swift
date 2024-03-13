8ec1c11eb48576eb56fb8edc7966b48a2c20631d
Ceren Çabık <ceren.cbk@hotmail.com>
2024-03-13 23:22:36 +0300

    update

---
+++ SimpsonBook/SimpsonBook/simpsoon.swift
@@ -0,0 +1,29 @@
+//
+//  simpsoon.swift
+//  SimpsonBook
+//
+//  Created by Ceren ÇABIK on 12.03.2024.
+//
+
+import UIKit
+
+class simpsoon: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
