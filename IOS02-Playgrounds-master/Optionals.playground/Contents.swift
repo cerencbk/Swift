79543894cfb5e52504ef2100a28fcf0496919c3a
Ceren Çabık <ceren.cbk@hotmail.com>
2024-03-20 17:11:49 +0300

    update

---
+++ IOS02-Playgrounds-master/Optionals.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var myName : String?
+
+myName?.uppercased()
+
+//optionals : ? vs !
+
+var myAge = "rr"
+
+
+var myInteger = (Int(myAge) ?? 0) * 5
+
+if let myNumber = Int(myAge) {
+    print(myNumber * 5)
+} else {
+    print("wrong input")
+}
+
+
+
+
+
