37b6904d5d4e287284c52565938a6846ff65c12a
Ceren Çabık <ceren.cbk@hotmail.com>
2024-03-21 23:07:54 +0300

    update

---
+++ ArtBookProject/ArtBookProject/DetailVC.swift
@@ -0,0 +1,28 @@
+//
+//  DetailVC.swift
+//  ArtBookProject
+//
+//  Created by Ceren ÇABIK on 21.03.2024.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var ArtistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+
+}
---
+++ ArtBookProject/ArtBookProject/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ArtBookProject
+//
+//  Created by Ceren ÇABIK on 21.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //En üst bara artı tuşu eklemek için
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        
+    }
+    
+    //Artı tuşuna basıldığında Segue yi yöblendirmek için
+    @objc func addButtonClicked(){
+        performSegue(withIdentifier: "toDetailVC", sender: nil)
+    }
+    
+    
+
+
+}
+
