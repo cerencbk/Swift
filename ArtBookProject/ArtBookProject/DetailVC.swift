d58cd4f4ee5df3eca2514837ee4c4647cbc6c39a
Ceren Çabık <ceren.cbk@hotmail.com>
2024-03-22 23:15:51 +0300

    update

--- ArtBookProject/ArtBookProject/DetailVC.swift
+++ ArtBookProject/ArtBookProject/DetailVC.swift
@@ -10,20 +10,86 @@ import CoreData
 
 class DetailVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var ArtistText: UITextField!
     @IBOutlet weak var yearText: UITextField!
     
-
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
+    var chosenPainting = ""
+    var chosenPaintingid : UUID?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if chosenPainting != ""{
+            
+            saveButton.isHidden = false
+            saveButton.isEnabled = false
+            
+            //Core Data
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = chosenPaintingid?.uuidString
+            //filtreleme için yani id si ..ya eşit olanı getir
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject] {
+                        
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+                        
+                        if let artist = result.value(forKey: "artist") as? String {
+                            ArtistText.text = artist
+                        }
+                        
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearText.text = String(year)
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                        
+                    }
+                }
+                
+            } catch{
+                print("error")
+            }
+            
+            
+            
+            
+        }else{
+            saveButton.isHidden = false
+            saveButton.isEnabled = false
+            nameText.text = ""
+            ArtistText.text = ""
+            yearText.text = ""
+        }
+        
         //Recognisers
         
         //klavyeyi boşluğa tıklandığında kapatmak için
         //Recognizers
-       
+        
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
         
@@ -41,9 +107,12 @@ class DetailVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     }
     
     
+    
     // görseli ekledikten sonra ne olacak eklemek zorundayız yoksa orda işlem kalır
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
         imageView.image = info[.originalImage] as? UIImage
+        saveButton.isEnabled = true
         self.dismiss(animated: true, completion: nil)
         
         print("hello")
@@ -54,32 +123,42 @@ class DetailVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     }
     
     @IBAction func saveButton(_ sender: Any) {
-        let appDelegeta = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegeta.persistentContainer.viewContext
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
         
         let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
         
-        //Attribute
-        newPainting.setValue(nameText.text!, forKey: "name")
-        newPainting.setValue(ArtistText.text, forKey: "artist")
+        //Attributes
         
-        if let year = Int(yearText.text!){
+        newPainting.setValue(nameText.text!, forKey: "name")
+        newPainting.setValue(ArtistText.text!, forKey: "artist")
+        
+        if let year = Int(yearText.text!) {
             newPainting.setValue(year, forKey: "year")
         }
         
         newPainting.setValue(UUID(), forKey: "id")
         
-        let data = imageView.image!.jpegData(compressionQuality: 0.5)   //Görseli data olarak eklemek için
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        
         newPainting.setValue(data, forKey: "image")
         
         do {
             try context.save()
-            print("Succes")
+            print("success")
         } catch {
-           print("error")
+            print("error")
         }
+        
+        
+        NotificationCenter.default.post(name: NSNotification.Name("newData"), object: nil)
+        self.navigationController?.popViewController(animated: true)
+        
     }
     
-
+    
+    
+    
     
 }
--- ArtBookProject/ArtBookProject/ViewController.swift
+++ ArtBookProject/ArtBookProject/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var nameArray = [String]()
     var idArray = [UUID]()
+    var selectedPainting = ""
+    var selectedPaintingid : UUID?
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -28,40 +30,55 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     
-    func getData() {
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "newData"), object: nil)
+    }
+    
+    
+    @objc func getData() {
+        
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
-        let fetchRecuest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
-        fetchRecuest.returnsObjectsAsFaults = false // casch ten okumayı kapatıyoruz programın hılanması için
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
         
         do {
-            let results = try context.fetch(fetchRecuest)
-            
-            for result in results as! [NSManagedObject]{
-                if let name = result.value(forKey: "name") as? String{
-                    self.nameArray.append(name)
-                }
-                if let id = result.value(forKey: "id") as? UUID{
-                    self.idArray.append(id)
-                }
-                
-                self.tableView.reloadData()
+            let results = try context.fetch(fetchRequest)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                                if let name = result.value(forKey: "name") as? String {
+                                    self.nameArray.append(name)
+                                }
+                                
+                                if let id = result.value(forKey: "id") as? UUID {
+                                    self.idArray.append(id)
+                                }
+                                
+                                self.tableView.reloadData()
+                                
+                            }
             }
             
+
         } catch {
             print("error")
         }
         
     }
+
     
     //Artı tuşuna basıldığında Segue yi yöblendirmek için
     @objc func addButtonClicked(){
+        selectedPainting = ""
         performSegue(withIdentifier: "toDetailVC", sender: nil)
     }
     
     
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return nameArray.count
     }
@@ -73,7 +90,73 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     
+    //segue 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailVC" {
+            let destinationVC = segue.destination as! DetailVC
+            destinationVC.chosenPainting = selectedPainting
+            destinationVC.chosenPaintingid = selectedPaintingid
+        }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPainting = nameArray[indexPath.row]
+        selectedPaintingid = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailVC", sender: nil)
+        
+    }
+    
+    
+    //Silme işlemi için kullanıcının ne işlem yaptığını algılıyor
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            //core data dan verilere erişmek için
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = idArray[indexPath.row].uuidString
+            //filtreleme için yani id si ..ya eşit olanı getir
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            
+            
+            //silme işleminin asıl gerçekleşmesi
+            do {
+                let results = try context.fetch(fetchRequest)
+                    if results.count > 0 {
+                        for result in results as! [NSManagedObject]{
+                            if let id = result.value(forKey: "id") as? UUID{
+                                if id == idArray[indexPath.row] {
+                                    context.delete(result)
+                                    nameArray.remove(at: indexPath.row)
+                                    idArray.remove(at: indexPath.row)
+                                    self.tableView.reloadData()
+                                    
+                                    
+                                    do {
+                                        try context.save()
+                                        
+                                    } catch{
+                                        print("error")
+                                    }
+                                    
+                                    print("deleted")
+                                    break
+                                }
+                            }
+                        }
+                    }
+                
+            }catch {
+                print("error")
+            }
+            
+            
+        } else if editingStyle == .insert {
+            // Burada, hücre ekleme işlemini gerçekleştirin.
+        }
+    }
 
- 
+    
+    
 }
-
